6ff4c630cc1defcb75acd06ce0650d2ea35129b5
chapp <chapp.hkg@gmail.com>
2020-04-25 22:55:34 +0800

    feat: add facility interface

--- src/app/home/facilities.service.ts
+++ src/app/home/facilities.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Facility } from './facility';
 
 @Injectable()
 export class FacilitiesService {
   constructor(private httpClient: HttpClient) {}
 
-  getBasketballCourts(): Observable<string> {
-    return this.httpClient.get<string>('/datagovhk/facility/facility-bkbc.json');
+  getBasketballCourts(): Observable<Facility> {
+    return this.httpClient.get<Facility>('/datagovhk/facility/facility-bkbc.json');
   }
 }
---
+++ src/app/home/facility.ts
@@ -0,0 +1,20 @@
+export interface Facility {
+  District_en: string;
+  District_cn: string;
+  Name_en: string;
+  Name_cn: string;
+  Address_en: string;
+  Address_cn: string;
+  GIHS: string;
+  Court_no_en: string;
+  Court_no_cn: string;
+  Ancillary_facilities_en: string;
+  Ancillary_facilities_cn: string;
+  Opening_hours_en: string;
+  Opening_hours_cn: string;
+  Phone: string;
+  Remarks_en: string;
+  Remarks_cn: string;
+  Longitude: string;
+  Latitude: string;
+}
