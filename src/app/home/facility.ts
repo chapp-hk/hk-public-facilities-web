33ba6e7286ff7f7f4434d5905ff2040f4c8c573b
chapp <chapp.hkg@gmail.com>
2020-04-26 21:43:02 +0800

    feat: add feature types

--- src/app/home/facility.ts
+++ src/app/home/facility.ts
@@ -6,15 +6,22 @@ export interface Facility {
   Address_en: string;
   Address_cn: string;
   GIHS: string;
-  Court_no_en: string;
-  Court_no_cn: string;
+  Court_no_en?: string;
+  Court_no_cn?: string;
+  Facilities_en?: string;
+  Facilities_cn?: string;
   Ancillary_facilities_en: string;
   Ancillary_facilities_cn: string;
   Opening_hours_en: string;
   Opening_hours_cn: string;
+  Maintenance_day_en?: string;
+  Maintenance_day_cn?: string;
   Phone: string;
-  Remarks_en: string;
-  Remarks_cn: string;
+  Remarks_en?: string;
+  Remarks_cn?: string;
+  Photo_1?: string;
+  Photo_2?: string;
+  Photo_3?: string;
   Longitude: string;
   Latitude: string;
 }
