48280b8c7b6886f21d2d3577ba78f2537920bc9e
chapp <chapp.hkg@gmail.com>
2020-04-30 00:02:49 +0800

    test: add unit tests for language selector component

--- src/app/i18n/language-selector.component.spec.ts
+++ src/app/i18n/language-selector.component.spec.ts
@@ -7,25 +7,54 @@ import { MaterialModule } from '@app/material.module';
 import { I18nService } from './i18n.service';
 import { LanguageSelectorComponent } from './language-selector.component';
 
+class MockI18nService {
+  private lang: string = '';
+  supportedLanguages: string[] = ['zh-HK', 'zh-TW'];
+
+  set language(language: string) {
+    this.lang = language;
+  }
+
+  get language(): string {
+    return this.lang;
+  }
+}
+
 describe('LanguageSelectorComponent', () => {
   let component: LanguageSelectorComponent;
   let fixture: ComponentFixture<LanguageSelectorComponent>;
+  let i18nService: I18nService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [BrowserAnimationsModule, FlexLayoutModule, MaterialModule, TranslateModule.forRoot()],
       declarations: [LanguageSelectorComponent],
-      providers: [I18nService],
+      providers: [{ provide: I18nService, useClass: MockI18nService }],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LanguageSelectorComponent);
+    i18nService = TestBed.inject(I18nService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('set language', () => {
+    component.setLanguage('en-US');
+    expect(i18nService.language).toEqual('en-US');
+  });
+
+  it('get current language should equal to i18n service language', () => {
+    i18nService.language = 'zh-HK';
+    expect(component.currentLanguage).toEqual(i18nService.language);
+  });
+
+  it('get languages should equal to i18n service supported languages', () => {
+    expect(component.languages).toEqual(i18nService.supportedLanguages);
   });
 });
